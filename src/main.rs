b5540b530892ae08d172ee88106a53f23aa49d40
andberger <and.bergsson@gmail.com>
2022-06-20 21:50:13 +0000

    Make CLI interface more friendly.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,31 @@
 use std::{env, fs};
+use std::panic::set_hook;
 use tinyserde::parser::JsonParser;
 use tinyserde::parser::JsonValue;
 use tinyserde::parser::ParserError;
 use tinyserde::deserializer::Deserializer;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let path = &args[1];
-    let paths = fs::read_dir(path).unwrap();
+const RESULT_FILE: &str = "data/combined.json";
+const MISSING_PATH_MESSAGE: &str = r#"Missing path parameter"#;
+const INCORRECT_USAGE_MESSAGE: &str = r#"Incorrect usage of tinyserde, please see tinyserde --help for more details"#;
+const HELP_MESSAGE: &str = r#"
+Tinyserde is a simple program that parses json files that reside in the same directory and combines them into a single file.
+
+The resulting output will be written to the file "data/combined.json".
+
+USAGE:
+    tinyserde [OPTIONS] --path <PATH>
+
+OPTIONS:
+    -h, --help             Print help information.
+    -p, --path <PATH>      Directory containing json files to combine.
+"#;
+
+fn combine_json(path: &str) -> () {
+    let files = fs::read_dir(path).expect("Could not find directory.");
     let mut parsed_objects: Vec<JsonValue> = vec![];
-    for path in paths {
-        let contents = fs::read_to_string(path.unwrap().path()).unwrap_or("".to_string());
+    for file in files {
+        let contents = fs::read_to_string(file.unwrap().path()).expect("Could not read file.");
         let mut parser = JsonParser {
             input: contents, 
             cursor: 0,
@@ -25,5 +40,31 @@ fn main() {
         input: JsonValue::Array(parsed_objects)
     };
     let json_as_string = deserializer.deserialize();
-    fs::write("data/combined.json", json_as_string).expect("Unable to write file");
+    fs::write(RESULT_FILE, json_as_string).expect("Unable to write to file.");
+}
+
+fn main() {
+    set_hook(Box::new(|info| {
+        if let Some(s) = info.payload().downcast_ref::<String>() {
+            println!("{}", s);
+        }
+    }));
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        println!("{}", INCORRECT_USAGE_MESSAGE);
+        return
+    }
+    let flag = &args[1];
+    if flag == "-h" || flag == "--help" {
+        println!("{}", HELP_MESSAGE);
+    } else if flag == "-p" || flag == "--path" {
+        if args.len() < 3 {
+            println!("{}", MISSING_PATH_MESSAGE);
+            return
+        }
+        let path = &args[2];
+        combine_json(path);
+    } else {
+        println!("{}", INCORRECT_USAGE_MESSAGE);
+    }
 }
